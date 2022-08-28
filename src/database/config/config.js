c24dedbab8c5b4b2f811ceeaa5a8503ad6e5420f
Fernanda9421 <fer.candrade21@gmail.com>
2022-08-28 14:14:10 -0300

    fix: altera configurações do banco de dados

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: "postgres",
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: "postgres",
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
