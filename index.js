231b4c420ea039ad182aa96c485b14ca5ac43c8a
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-02 07:11:28 -0300

    feat: adiciona rota /appointment no index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const payment = require('./src/routes/payments.router');
+const appointment = require('./src/routes/appointments.router');
 const errorMiddleware = require('./src/middlewares/error.middleware');
 
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/payment', payment);
+app.use('/appointment', appointment);
 
 app.use(errorMiddleware);
 
