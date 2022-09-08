692877fcd7a32190ca8fa33e51855795ad4eee2b
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-08 13:24:27 -0300

    feat: cria código de erro para conflitos

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,7 +1,8 @@
-module.exports = (err, _req, res, next) => {
+module.exports = (err, _req, res, _next) => {
   if (err.code) {
     const statusByErrorCode = {
       notFound: 404,
+      alreadyExists: 409,
     };
 
     const status = statusByErrorCode[err.code] || 500;
